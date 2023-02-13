aa92ab4dfddf4b6679cb84bcc29b0c97aa9fbba6
kgklein <kris.klein@gmail.com>
2023-02-13 10:23:04 -0700

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 cd distribution
 ./generate_distribution.e test_kpar_v00.in
 cd ../
-mpirun -np 8 --oversubscribe ./ALPS.e test_kpar_fast.in
+mpirun -np 8 --oversubscribe ./src/ALPS test_kpar_fast.in
