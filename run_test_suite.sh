b53161d311223b6cb5a2db22f87a637dfb445bf8
kgklein <kris.klein@gmail.com>
2023-02-17 09:49:06 -0700

    added analytical test inputs

--- run_test_suite.sh
+++ run_test_suite.sh
@@ -209,7 +209,39 @@ else
 fi
 date
 
-if [ $jj0 -eq 0 ] && [ $kk0 -eq 0 ] && [ $jj1 -eq 0 ] && [ $kk1 -eq 0 ] && [ $jj2 -eq 0 ] && [ $kk2 -eq 0 ] && [ $jj3 -eq 0 ] && [ $kk3 -eq 0 ] && [ $jj4 -eq 0 ] && [ $kk4 -eq 0 ] && [ $jj5 -eq 0 ] && [ $kk5 -eq 0 ]
+echo '-=-=-=-=-=-=-='
+echo ''
+echo '-=-=-=-=-=-=-='
+input='test_analytical'
+echo 'Testing: ' $input
+echo '-=-=-=-=-=-=-='
+echo 'Generate Distribution'
+date
+cd distribution
+./generate_distribution $input'_dist.in' 2> $input'_dist.error' > $input'_dist.out'
+jj6=$(wc -l $input'_dist.error' | awk '{print $1}')
+if [ $jj6 -eq 0 ]
+then
+    echo 'No Errors in generate_distribution execution of' $input
+else
+    echo 'ERRORS in generate_distribution execution of' $input
+fi
+date
+cd ../
+echo '-=-=-=-=-=-=-='
+echo 'ALPS'
+date
+mpirun -np $numproc --oversubscribe ./src/ALPS $input.in 2> $input'.error' > $input'.out'
+kk6=$(wc -l $input'.error' | awk '{print $1}')
+if [ $kk6 -eq 0 ]
+then
+    echo 'No Errors for ALPS Execution of ' $input
+else
+    echo "ERRORS in ALPS Execution of" $input
+fi
+date
+
+if [ $jj0 -eq 0 ] && [ $kk0 -eq 0 ] && [ $jj1 -eq 0 ] && [ $kk1 -eq 0 ] && [ $jj2 -eq 0 ] && [ $kk2 -eq 0 ] && [ $jj3 -eq 0 ] && [ $kk3 -eq 0 ] && [ $jj4 -eq 0 ] && [ $kk4 -eq 0 ] && [ $jj5 -eq 0 ] && [ $kk5 -eq 0 ] && [ $jj6 -eq 0 ] && [ $kk6 -eq 0 ]
 then
     echo "No errors in ALPS Execution of Test Suite. Congratulations."
 else
